1c1141e10854007825015555981eda9c8e981f03
Ania <ania.makes@gmail.com>
2018-01-25 15:46:39 +0000

    person completes the maze! Other functionality still needs to be implemented

--- maze.js
+++ maze.js
@@ -46,8 +46,7 @@ function personPositionFn() {
       var xPosition = maze[yAxisIterator].findIndex(findPerson);
       if (xPosition != -1) {
         return [yAxisIterator, xPosition];
-      }
-      else {
+      } else {
         continue;
       }
     }
@@ -70,7 +69,7 @@ function personFacingFn() {
 function surroundingsFn() {
 
   var currentPosition = personPositionFn();
-  if(!currentPosition){
+  if (!currentPosition) {
     debugger;
   }
   var yAxisPosition = currentPosition[0];
@@ -97,7 +96,7 @@ function surroundingsFn() {
 
 console.log(surroundingsFn());
 
-function orient(){
+function orient() {
   var currentPosition = personPositionFn();
   var yAxisPosition = currentPosition[0];
   var xAxisPosition = currentPosition[1];
@@ -113,14 +112,11 @@ function orient(){
 function nextDirection(direction) {
   if (direction == "north") {
     return "^";
-  }
-  else if (direction == "east") {
+  } else if (direction == "east") {
     return ">";
-  }
-  else if (direction == "south"){
+  } else if (direction == "south") {
     return "v";
-  }
-  else if (direction == "west"){
+  } else if (direction == "west") {
     return "<";
   }
 }
@@ -136,93 +132,110 @@ function whereToGo() {
   console.log(surroundings, personFacing);
 
   if (personFacing == "^") {
-    if (surroundings.north !== "X") {
-      return [surroundings.northCoord, surroundings.north, "north"];
-    } else if (surroundings.west !== "X") {
-      return [surroundings.westCoord, surroundings.west, "west"];
-    }
-  } else if (personFacing == ">") {
     if (surroundings.east !== "X") {
       return [surroundings.eastCoord, surroundings.east, "east"];
     } else if (surroundings.north !== "X") {
       return [surroundings.northCoord, surroundings.north, "north"];
-    }
-  } else if (personFacing == "v") {
-    if (surroundings.south !== "X") {
-      return [surroundings.southCoord, surroundings.south, "south"];
-    } else if (surroundings.north !== "X") {
-      return [surroundings.eastCoord, surroundings.east, "east"];
-    }
-  } else if (personFacing == "<") {
-    if (surroundings.west !== "X") {
+    } else if (surroundings.west !== "X") {
       return [surroundings.westCoord, surroundings.west, "west"];
-    } else if (surroundings.south !== "X") {
+    } else {
       return [surroundings.southCoord, surroundings.south, "south"];
     }
   }
+  else if (personFacing == ">") {
+    if (surroundings.south !== "X") {
+      return [surroundings.southCoord, surroundings.south, "south"];
+    } else if (surroundings.east !== "X") {
+      return [surroundings.eastCoord, surroundings.east, "east"];
+    } else if (surroundings.north !== "X") {
+      return [surroundings.northCoord, surroundings.north, "north"];
+    }else {
+      return [surroundings.westCoord, surroundings.west, "west"];
+    }}
+    else if (personFacing == "v") {
+      if (surroundings.west !== "X") {
+        return [surroundings.westCoord, surroundings.west, "west"];
+      } else if (surroundings.south !== "X") {
+        return [surroundings.southCoord, surroundings.south, "south"];
+      } else if (surroundings.east !== "X") {
+        return [surroundings.eastCoord, surroundings.east, "east"];
+      } else {
+        return [surroundings.northCoord, surroundings.north, "north"];
+      }
+    }
+    else if (personFacing == "<") {
+      if (surroundings.north !== "X") {
+        return [surroundings.northCoord, surroundings.north, "north"];
+      } else if (surroundings.west !== "X") {
+        return [surroundings.westCoord, surroundings.west, "west"];
+      } else if (surroundings.south !== "X") {
+        return [surroundings.southCoord, surroundings.south, "south"];
+      } else {
+        return [surroundings.eastCoord, surroundings.east, "east"];
+      }
+    }
+
 }
 
-function move(){
+function move() {
   var destination = whereToGo();
   var coordinatesOfMove = destination[0];
   var coordinatesOfMoveY = coordinatesOfMove[0];
   var coordinatesOfMoveX = coordinatesOfMove[1];
   var emptyOrExit = destination[1];
   var directionOfMove = destination[2];
-  console.log (coordinatesOfMove, emptyOrExit, directionOfMove);
+  console.log(coordinatesOfMove, emptyOrExit, directionOfMove);
 
   var personPosition = personPositionFn();
   var personPreviousY = personPosition[0];
   var personPreviousX = personPosition[1];
 
   // move empty space to where the person was
- maze[personPreviousY][personPreviousX] = " ";
+  maze[personPreviousY][personPreviousX] = " ";
 
   // move person to where the space was
-var newPersonDirection = "";
+  var newPersonDirection = "";
 
-if (directionOfMove == "north") {
-  newPersonDirection = "^";
-} else if ( directionOfMove == "east") {
-  newPersonDirection = ">";
-} else if (directionOfMove == "south") {
-  newPersonDirection = "v";
-} else if (directionOfMove == "west") {
-  newPersonDirection = "<";
-}
+  if (directionOfMove == "north") {
+    newPersonDirection = "^";
+  } else if (directionOfMove == "east") {
+    newPersonDirection = ">";
+  } else if (directionOfMove == "south") {
+    newPersonDirection = "v";
+  } else if (directionOfMove == "west") {
+    newPersonDirection = "<";
+  }
 
-maze[coordinatesOfMoveY][coordinatesOfMoveX] = newPersonDirection;
+  maze[coordinatesOfMoveY][coordinatesOfMoveX] = newPersonDirection;
 
-journey.push(personPosition);
-// TODO implement clearing console before printing the new maze
-console.log(maze);
-stepsSoFar ++;
+  journey.push(personPosition);
+  // TODO implement clearing console before printing the new maze
+  console.log(maze);
+  stepsSoFar++;
 
-if(emptyOrExit == "E") {
-  journey.push(coordinatesOfMove);
-  return "completed";
-} else {
-  console.log ("Steps so far: ${stepsSoFar}");
-  return "continue";
-}
+  if (emptyOrExit == "E") {
+    journey.push(coordinatesOfMove);
+    return "completed";
+  } else {
+    console.log("Steps so far: ${stepsSoFar}");
+    return "continue";
+  }
 
   // console log the maze
 
 }
 
-function walkTheMaze(){
+function walkTheMaze() {
   orient();
   console.log(maze);
   whereToGo();
-  debugger;
-  if (move() != "completed"){
-    move();
+
+  var whileChecker = "";
+
+  while (whileChecker != "completed") {
+    whileChecker = move();
   }
-  else {
-    console.log("FINISHED");
-    console.log(journey);
-    console.log(stepsSoFar);
-  }
+
 }
 
 walkTheMaze();
